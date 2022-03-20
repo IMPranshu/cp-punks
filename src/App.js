ef62a705d7eb03dde9585acc01df4e8810087336
P K Agrawal <pranshukumar1999@gmail.com>
2022-03-20 12:00:29 +0530

    Body of page started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CollectionCard from './components/CollectionCard';
 import { Header } from './components/Header';
 
 function App() {
     return( 
     <div className="app">
       <Header/>
+      <CollectionCard  id={0} name={'HeadBand Punk'} traits={[{'value' :7}]} image='https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg' />
     </div>
     )
 }
